4db0d7c561baf30b104c69cadbaa0902a27a2057
Maurizio Sodano <maurizio.sodano@gmail.com>
2024-02-27 12:26:32 +0100

    added update and delete operation

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:faker/faker.dart';
+import 'package:faker/faker.dart' hide Job;
 import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
 import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../data/firestore_repository.dart';
+import '../data/job.dart';
 import 'firebase_auth_provider.dart';
 
 class HomeScreen extends ConsumerWidget {
@@ -45,12 +46,34 @@ class JobsListView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final firestoreRepository = ref.watch(firestoreRepositoryProvider);
-    return FirestoreListView(
+    return FirestoreListView<Job>(
       query: firestoreRepository.jobsQuery(),
       itemBuilder: (context, doc) {
-        return ListTile(
-          title: Text(doc['title']),
-          subtitle: Text(doc['company']),
+        final job = doc.data();
+        return Dismissible(
+          key: Key(doc.id),
+          background: const ColoredBox(color: Colors.red),
+          direction: DismissDirection.endToStart,
+          onDismissed: (direction) {
+            final user = ref.read(firebaseAuthProvider).currentUser;
+            ref.read(firestoreRepositoryProvider).deleteJob(user!.uid, doc.id);
+          },
+          child: ListTile(
+            title: Text(job.title),
+            subtitle: Text(job.company),
+            onTap: () {
+              final user = ref.read(firebaseAuthProvider).currentUser;
+              final faker = Faker();
+              final title = faker.job.title();
+              final company = faker.company.name();
+              ref.read(firestoreRepositoryProvider).updateJob(
+                    user!.uid,
+                    doc.id,
+                    title,
+                    company,
+                  );
+            },
+          ),
         );
       },
     );
